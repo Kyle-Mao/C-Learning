6b960e218f4f9b8e563e563aaca451c1e92ac549
KyleMao <69608005+Kyle-Mao@users.noreply.github.com>
2023-10-31 17:35:32 +0800

    Encoder v1.4

--- Encoder.c
+++ Encoder.c
@@ -1,14 +1,15 @@
-// C Program by Kyle. 20231024 10:04
-// v1.1 允许空格输入
+// C Program by Kyle. 2023102 14:31
+// v1.4 解决功能选择输入无效值时循环报错问题    解决16进制输出第一位是0的问题
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#define LENGTH 1000
 
 int main()
 {
     int choice, raw, pw, len;
-    char str[1000] = {};
+    char str[LENGTH] = {};
     printf("1-Encode\n");
     printf("2-Decode\n");
     printf("Type your choice: ");
@@ -25,6 +26,7 @@ option:
         break;
     default:
         printf("Invalid. Type again:");
+        fflush(stdin);
         goto option;
     }
 
@@ -32,7 +34,7 @@ encode:
     printf("Input your text to be encoded:\n");
     fflush(stdin);
     // scanf("%s", &str);
-    fgets(str, 1000, stdin);
+    fgets(str, LENGTH, stdin);
     // scanf("%[^\n]",&str);
     printf("\nInput your password: \n");
     scanf("%d", &raw);
@@ -40,16 +42,31 @@ encode:
     len = strlen(str);
     pw = raw % 128;
 
-    char encode[1000] = {};
-    // printf("%s", str);
-    // printf("%d", pw);
+    char encode[LENGTH] = {};
     printf("\nPlease copy your result: \n");
     for (int i = 0; i < len; i++)
     {
-        // printf("%x\n", str[i]);
+
         encode[i] = str[i] ^ pw;
-        // printf("%x\n", encode[i]);
-        printf("%x", encode[i]);
+        // printf("%x", encode[i]);
+
+        if (encode[i] < 16)
+        {
+            printf("%d", 0);
+
+            if (encode[i] == 0)
+            {
+                printf("%d", 0);
+            }
+            else
+            {
+                printf("%x", encode[i]);
+            }
+        }
+        else
+        {
+            printf("%x", encode[i]);
+        }
     }
     printf("\n");
     scanf("%d", choice);
@@ -57,10 +74,10 @@ encode:
 
 decode:
 
-    char str2[1000][2] = {};
-    char format[1000][3] = {};
-    char pretext[1000] = {};
-    char decode[1000] = {};
+    char str2[LENGTH][2] = {};
+    char format[LENGTH][3] = {};
+    char pretext[LENGTH] = {};
+    char decode[LENGTH] = {};
     printf("Input your text to be decoded:\n");
     scanf("%s", &str2);
     printf("\nInput your password: \n");
@@ -74,27 +91,14 @@ decode:
     {
         format[i][0] = str2[i][0];
         format[i][1] = str2[i][1];
-        // format[i][2] = "\0";
-        // char str2[i][2] = "\0";
-        // printf("%.2s\n", format[i]);
 
-        // printf("%s\n", format[i]);
-
-        // sscanf(string, "%x", &data);
-        // decode[i] = str2[i] ^ pw;
         char *str = format[i];
         char *endptr;
         long int hex_value;
 
         // 使用 strtol 函数将字符串转换为整数，基数为 16（十六进制）
         hex_value = strtol(str, &endptr, 16);
-        // if (errno == ERANGE || *endptr != '\0')
-        // {
-        //     printf("转换失败\n");
-        //     return 1;
-        // }
 
-        // printf("十六进制整数：%ld\n", hex_value);
         pretext[i] = hex_value;
         decode[i] = hex_value ^ pw;
     }
